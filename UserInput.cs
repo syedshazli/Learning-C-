0658ba6af78ddfa0bf6415101e7001d054e169af
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-27 11:57:00 -0400

    Update UserInput.cs

--- UserInput.cs
+++ UserInput.cs
@@ -11,7 +11,7 @@ namespace UserInput
         static void Main(string[] args)
         {
             Console.WriteLine("Now that I've said hello to the world, let's introduce ourselves. I'm Syed, you must be..");
-            string name = Console.ReadLine();
+            string name = Convert.ToString(Console.ReadLine());
             Console.WriteLine("Hi " + name + ", it's nice meeting you!");
 
         }

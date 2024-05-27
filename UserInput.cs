4617a524fb86dc8179e51a968776f58bdfc24609
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-27 11:49:51 -0400

    Create UserInput.cs

---
+++ UserInput.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Now that I've said hello to the world, let's introduce ourselves. I'm Syed, you must be..");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name + ", it's nice meeting you!");
+
+        }
+    }
+}

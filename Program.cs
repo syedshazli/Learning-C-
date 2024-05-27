cc71eb65beea7e4781844d80e3144b79c2d28645
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2024-05-27 09:35:23 -0400

    Create Program.cs
    Hello World!

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloC_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
